41fb4b0080bf3821e70c5dfed877e610fe9ce741
Prathmesh2001 <prathmeshw1102@gmail.com>
2021-11-21 17:34:34 +0530

    components and routers added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './CSS/styles.css';
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Navbar from './components/Navbar';
+import Auth from './components/Auth';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Navbar/>
+        <div>
+          <Routes>
+            <Route exact path="/home" element={<Home />}></Route>
+            <Route exact path="/profile" element={<Profile />}></Route>
+            <Route exact path="/" element={<Auth />}></Route>
+            <Route path="/" element={<div>404</div>}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
   );
 }
 
---
+++ frontend/src/components/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Auth() {
+    return (
+        <div className='btn btn-primary'>
+            Auth
+        </div>
+    )
+}
+
+export default Auth
